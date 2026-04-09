15c36c89afed82b2807d55f8f44a6b7cc74f0cc8
Javier <javiernaranjosimarro1@gmail.com>
2026-04-09 16:33:35 +0200

    v2.2: frontend actualizado y renovado

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -91,7 +91,7 @@ resource "aws_lb" "app_alb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
 
-  subnets = slice(data.aws_subnets.public.ids, 0, 2)
+  subnets = data.aws_subnets.public.ids
 }
 
 resource "aws_lb_listener" "https" {
